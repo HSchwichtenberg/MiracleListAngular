479d74179d64e29625ef4fdb0f33ea6e46f08f84
dotnetdoktor <hs@IT-Visions.de>
2017-02-16 23:29:46 +0100

    Verbesserung des Workadround für jQuery in Electron

--- src/electron/jquery-fix.js
+++ src/electron/jquery-fix.js
@@ -1,3 +1,12 @@
-//vgl. https://github.com/electron/electron/issues/254
+//vgl. https://github.com/electron/electron/issues/254 und http://stackoverflow.com/questions/32621988/electron-jquery-is-not-defined
 
-if (!window.$) { window.$ = window.jQuery = require('jquery'); }
+if (!window.$) {
+    console.log("jQuery fix...")
+    if (typeof module === 'object') {
+        window.module = module;
+        module = undefined;
+    }
+    window.$ = window.jQuery = require('./jquery.min.js');
+    if (window.module) module = window.module;
+    console.log("jQuery fix END")
+}
