e5c026636068792c21b7f84c5a90bf6fe79e7e54
dotnetdoktor <hs@IT-Visions.de>
2017-02-17 01:04:39 +0100

    f

--- src/Start/Start.component.ts
+++ src/Start/Start.component.ts
@@ -10,42 +10,42 @@ import { Modal } from 'angular2-modal/plugins/bootstrap';
 // MomentJS
 import * as moment from 'moment';
 
-declare var electron: any;  
+declare var electron: any;
 // Versionsnummer auslesen
 var pckg = require('../../package.json');
-// 
 
 @Component({
- selector: 'Start',
- templateUrl: './Start.component.html'
+    selector: 'Start',
+    templateUrl: './Start.component.html'
 })
 export class StartComponent implements OnInit {
 
- ngOnInit() { console.log("======= LoginComponent:ngOnInit");
-    console.log("Anwendung: " + pckg.name);
-    console.log("Version: " + pckg.version +  " vom " + pckg.date);
-    console.log("Sprache: " + (<any>moment().localeData())._abbr); 
-}
+    ngOnInit() {
+        console.log("======= LoginComponent:ngOnInit");
+        console.log("Anwendung: " + pckg.name);
+        console.log("Version: " + pckg.version + " vom " + pckg.date);
+        console.log("Sprache: " + (<any>moment().localeData())._abbr);
+    }
 
- constructor(private miracleListProxy : MiracleListProxy, private communicationService: CommunicationService, overlay: Overlay, vcr: ViewContainerRef, public modal: Modal, ) {
-  overlay.defaultViewContainer = vcr;
- }
+    constructor(private miracleListProxy: MiracleListProxy, private communicationService: CommunicationService, overlay: Overlay, vcr: ViewContainerRef, public modal: Modal, ) {
+        overlay.defaultViewContainer = vcr;
+    }
 
-//  get username(): string {
-//   return (this.communicationService.username)
-//  }
+    //  get username(): string {
+    //   return (this.communicationService.username)
+    //  }
 
- get isLoggedIn(): boolean {
-  return (this.communicationService.username != null && this.communicationService.username != "")
- }
+    get isLoggedIn(): boolean {
+        return (this.communicationService.username != null && this.communicationService.username != "")
+    }
 
- about() {
+    about() {
 
-  this.modal.alert()
-   .size('lg')
-   .showClose(true)
-   .title('Über die Anwendung MiracleList')
-   .body(`
+        this.modal.alert()
+            .size('lg')
+            .showClose(true)
+            .title('Über die Anwendung MiracleList')
+            .body(`
             <h4>Dies ist eine Beispielanwendung für eine Single-Page-Webapplication (SPA) zur Aufgabenverwaltung.</h4>
             <div>Autor: Dr. Holger Schwichtenberg, <a href="http://www.IT-Visions.de">www.IT-Visions.de</a></div>
             <div>Version: ${pckg.version} vom ${pckg.date}</div>
@@ -76,37 +76,20 @@ export class StartComponent implements OnInit {
                 <li>Electron-Version: ${this.getElectronVersion()}</li>
                 <li>Spracheinstellungen: Anwendung: ${(<any>moment().localeData())._abbr + " / Browser: " + window.navigator.language}</li>
             </ul>`
-   )
-   .open();
+            )
+            .open();
 
- }
- 
-isElectron() : boolean {
+    }
 
-var r = electron.remote;
-return true;
-// return ( (typeof r.process !== "undefined") && r.process.versions && ((<any>r.process.versions).electron !== undefined) )
+    getElectronVersion(): string {
+        if (!electron) return "n/a";
+        return (<any>electron.remote.getCurrentWindow()).version;
+    }
+
+    logout() {
+        console.log("logout");
+        this.miracleListProxy.logoff(this.communicationService.token).subscribe(x => x);
+        this.communicationService.token = "";
+        this.communicationService.navigate(""); // Ansicht aufrufen
+    }
 }
- getElectronVersion() : string {
-// var r = require('electron').remote;
-// var r = electron.remote;
-  return (<any>electron.remote.getCurrentWindow()).version;
-}
-
- logout() {
-  console.log("logout");
-  this.miracleListProxy.logoff(this.communicationService.token).subscribe(x=>x);
-  this.communicationService.token = "";
-  this.communicationService.navigate(""); // Ansicht aufrufen
- }
-}
-
-// modal.alert()
-//     .size('lg')
-//     .isBlocking(true)
-//     .showClose(true)
-//     .keyboard(27)
-//     .title('Hello World')
-//     .body('A Customized Modal')
-//     .open();
-
