75da3f7d64804405790f68efd0e14e84a7bd4c4c
dotnetdoktor <hs@IT-Visions.de>
2017-02-28 19:30:22 +0100

    Reorg Electron Main Prozess

---
+++ src/electron/MiracleListMenu.ts
@@ -0,0 +1,131 @@
+/**
+ * Created by HS on 28.02.2017.
+ */
+
+export class MiracleListMenu {
+// Menü erstellen
+ public static CreateMenu(app : Electron.App, win: Electron.BrowserWindow): Electron.MenuItemOptions[] {
+  let contents = win.webContents;
+  // Menü setzen
+  const menuTemplate: Electron.MenuItemOptions = [{
+   label: 'Anwendung',
+   submenu: [{
+    label: 'Über diese Anwendung',
+    click: () => {
+
+     // const options = {
+     //     type: 'info',
+     //     title: 'Desktop-Variante der Beispielanwendung MiracleList',
+     //     buttons: ['Ok'],
+     //     message: '(C) Dr. Holger Schwichtenberg 2017, Details siehe Hamburgermenü!'
+     // }
+     // dialog.showMessageBox(options, function() {})
+     console.log("Sende nachricht...");
+     contents.send('about', {msg: 'nachricht'});
+     console.log("Sende nachricht ENDE");
+     // console.log(options.message);
+    }
+   },
+    {
+     label: 'Abmelden',
+     click: () => {
+      contents.send('logout', {msg: ''});
+     }
+    },
+    {
+     label: 'Beenden',
+     click: () => {
+      app.quit();
+     }
+    }
+   ]
+  },
+   {
+    label: 'Bearbeiten',
+    submenu: [{
+     label: 'Undo',
+     accelerator: 'CmdOrCtrl+Z',
+     role: 'undo'
+    }, {
+     label: 'Redo',
+     accelerator: 'Shift+CmdOrCtrl+Z',
+     role: 'redo'
+    }, {
+     type: 'separator'
+    }, {
+     label: 'Ausschneiden',
+     accelerator: 'CmdOrCtrl+X',
+     role: 'cut'
+    }, {
+     label: 'Kopieren',
+     accelerator: 'CmdOrCtrl+C',
+     role: 'copy'
+    }, {
+     label: 'Einfügen',
+     accelerator: 'CmdOrCtrl+V',
+     role: 'paste'
+    }, {
+     label: 'Alles auswählen',
+     accelerator: 'CmdOrCtrl+A',
+     role: 'selectall'
+    }]
+   } // Ende Edit Menü
+   ,
+   {
+    label: 'Ansicht',
+    submenu: [
+     // {
+     // label: 'Reload',
+     // accelerator: 'CmdOrCtrl+R',
+     // click: function(item, focusedWindow) {
+     //     if (focusedWindow) {
+     //         // on reload, start fresh and close any old
+     //         // open secondary windows
+     //         if (focusedWindow.id === 1) {
+     //             BrowserWindow.getAllWindows().forEach(function(win) {
+     //                 if (win.id > 1) {
+     //                     win.close()
+     //                 }
+     //             })
+     //         }
+     //         focusedWindow.reload()
+     //     }
+     // }
+     // },
+     {
+      label: 'Zwischem Vollbildmodus und normalen Modus wechseln. ',
+      accelerator: (function () {
+       if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F';
+       } else {
+        return 'F11';
+       }
+      })(),
+      click: function (item, focusedWindow) {
+       if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+       }
+      }
+     }, {
+      label: 'Developer Tools Ein-/Ausblenden',
+      accelerator: (function () {
+       if (process.platform === 'darwin') {
+        return 'Alt+Command+I';
+       } else {
+        return 'F12';
+       }
+      })(),
+      click: function (item, focusedWindow) {
+       if (focusedWindow) {
+        focusedWindow.toggleDevTools();
+       }
+      }
+     }
+    ]
+   } // Ende View Menü
+
+  ];
+
+  return menuTemplate;
+ }
+}
--- src/electron/electron-main.ts
+++ src/electron/electron-main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow, Menu, dialog, ipcMain, Tray } from "electron";
+import { app, BrowserWindow, Menu, dialog, ipcMain, Tray, screen } from "electron";
+import {MiracleListMenu} from "./MiracleListMenu";
 
 const path = require('path');
 const url = require('url');
@@ -9,9 +10,11 @@ let win: Electron.BrowserWindow;
 
 function createWindow() {
 
-    // Create the electron browser window. 
+    // Create the electron browser window
     console.log("!!!Electron/Main:createWindow");
-    console.log("Path=" + __dirname);
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize
+    console.log("Screen:" + width + "x" + height );
+    console.log("Path:" + __dirname);
     win = new BrowserWindow({
         width: 900,
         height: 600,
@@ -38,7 +41,7 @@ function createWindow() {
     // TODO: new Tray: https://github.com/electron/electron/blob/master/docs/api/tray.md
     // TODO: Globaler Shortcut: https://github.com/electron/electron/blob/master/docs/api/global-shortcut.md
 
-    // Tray setzen
+    // // =================== Tray setzen
 
     let tray = null;
 
@@ -52,132 +55,13 @@ function createWindow() {
     tray.setToolTip('MiracleList');
     tray.setContextMenu(contextMenu);
 
-
-    // Menü setzen
-    const menuTemplate: any = [{
-        label: 'Anwendung',
-        submenu: [{
-            label: 'Über diese Anwendung',
-            click: () => {
-
-                // const options = {
-                //     type: 'info',
-                //     title: 'Desktop-Variante der Beispielanwendung MiracleList',
-                //     buttons: ['Ok'],
-                //     message: '(C) Dr. Holger Schwichtenberg 2017, Details siehe Hamburgermenü!'
-                // }
-                // dialog.showMessageBox(options, function() {})
-                console.log("Sende nachricht...");
-                contents.send('about', { msg: 'nachricht' });
-                console.log("Sende nachricht ENDE");
-                // console.log(options.message);
-            }
-        },
-        {
-            label: 'Abmelden',
-            click: () => {
-                contents.send('logout', { msg: '' });
-            }
-        },
-        {
-            label: 'Beenden',
-            click: () => {
-                app.quit();
-            }
-        }
-        ]
-    },
-    {
-        label: 'Bearbeiten',
-        submenu: [{
-            label: 'Undo',
-            accelerator: 'CmdOrCtrl+Z',
-            role: 'undo'
-        }, {
-            label: 'Redo',
-            accelerator: 'Shift+CmdOrCtrl+Z',
-            role: 'redo'
-        }, {
-            type: 'separator'
-        }, {
-            label: 'Cut',
-            accelerator: 'CmdOrCtrl+X',
-            role: 'cut'
-        }, {
-            label: 'Copy',
-            accelerator: 'CmdOrCtrl+C',
-            role: 'copy'
-        }, {
-            label: 'Paste',
-            accelerator: 'CmdOrCtrl+V',
-            role: 'paste'
-        }, {
-            label: 'Select All',
-            accelerator: 'CmdOrCtrl+A',
-            role: 'selectall'
-        }]
-    } // Ende Edit Menü
-        ,
-    {
-        label: 'Ansicht',
-        submenu: [
-            // {
-            // label: 'Reload',
-            // accelerator: 'CmdOrCtrl+R',
-            // click: function(item, focusedWindow) {
-            //     if (focusedWindow) {
-            //         // on reload, start fresh and close any old
-            //         // open secondary windows
-            //         if (focusedWindow.id === 1) {
-            //             BrowserWindow.getAllWindows().forEach(function(win) {
-            //                 if (win.id > 1) {
-            //                     win.close()
-            //                 }
-            //             })
-            //         }
-            //         focusedWindow.reload()
-            //     }
-            // }
-            // }, 
-            {
-                label: 'Zwischem Vollbildmodus und normalen Modus wechseln. ',
-                accelerator: (function () {
-                    if (process.platform === 'darwin') {
-                        return 'Ctrl+Command+F';
-                    } else {
-                        return 'F11';
-                    }
-                })(),
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-                    }
-                }
-            }, {
-                label: 'Developer Tools Ein-/Ausblenden',
-                accelerator: (function () {
-                    if (process.platform === 'darwin') {
-                        return 'Alt+Command+I';
-                    } else {
-                        return 'F12';
-                    }
-                })(),
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        focusedWindow.toggleDevTools();
-                    }
-                }
-            }
-        ]
-    } // Ende View Menü
-
-    ];
-
+    // =================== Anwendungsmenü einbinden
+    var menuTemplate = MiracleListMenu.CreateMenu(app, win);
     const menu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(menu);
 
     // Open the DevTools.
-    // win.webContents.openDevTools()
+    win.webContents.openDevTools()
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -215,3 +99,4 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
