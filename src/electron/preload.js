122c528716a7a7534b1e657f628f52c96b1e854a
dotnetdoktor <hs@IT-Visions.de>
2017-02-28 23:37:39 +0100

    Reorg: /src/electron ->/electron

--- src/electron/preload.js
+++
@@ -1,5 +0,0 @@
-console.log("preload.js", typeof require, typeof electron);
-if (typeof require !== "undefined") {
-    var electron = require('electron');
-}
-console.log("preload.js END", typeof require, typeof electron);
