3c8dee9e1830f94e0f737563d5dc2e188f84b88f
dotnetdoktor <hs@IT-Visions.de>
2017-03-01 11:11:33 +0100

    Problem mit Tray-Icon unter Linux abgefangen; Logging verbessert

--- electron/electron-main.ts
+++ electron/electron-main.ts
@@ -1,5 +1,5 @@
-import {app, BrowserWindow, Menu, dialog, ipcMain, Tray, screen} from "electron";
-import {MiracleListAppMenu} from "./MiracleListAppMenu";
+import { app, BrowserWindow, Menu, dialog, ipcMain, Tray, screen } from "electron";
+import { MiracleListAppMenu } from "./MiracleListAppMenu";
 import * as username from "username";
 import ShowMessageBoxOptions = Electron.ShowMessageBoxOptions;
 import * as fs from "fs";
@@ -7,30 +7,38 @@ import * as moment from "moment";
 
 const path = require('path');
 const url = require('url');
+const NativeImage = require('native-image');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win: Electron.BrowserWindow;
 
 function createWindow() {
+ console.log("createWindow");
 
  // Create the electron browser window
- console.log("!!!Electron/Main:createWindow");
+ writeLog("!!!Electron/Main:createWindow");
  const {width, height} = screen.getPrimaryDisplay().workAreaSize;
 
- console.log("OS:", process.platform);
- console.log("Screen:" + width + "x" + height);
- console.log("Anwendungspfad:" + __dirname);
- console.log("Aktueller Benutzer:" + username.sync());
- console.log("User Home Dir:" + app.getPath("documents"));
- writeLog('Anwendungstart');
+ writeLog("OS:" + process.platform);
+ writeLog("Screen:" + width + "x" + height);
+ writeLog("Anwendungspfad:" + __dirname);
+ writeLog("Aktueller Benutzer:" + username.sync());
+ writeLog("User Home Dir:" + app.getPath("documents"));
 
- var favicon : string = path.join(__dirname, 'favicon.ico');
+ const favicon: string = path.join(__dirname, 'favicon.ico');
+ writeLog("Icon1:" + favicon);
+ const icon: string = path.join(__dirname, 'favicon.png');
+ writeLog("Icon2:" + icon);
+
+const ni = NativeImage.createFromPath(favicon);
+
+ writeLog("new BrowserWindow()");
  win = new BrowserWindow({
   width: 900,
   height: 600,
   frame: true, // false für frameless Window
-  icon: favicon,
+  icon: ni,
   webPreferences: {
    nodeIntegration: true,
    preload: path.join(__dirname, 'preload.js')
@@ -41,14 +49,13 @@ function createWindow() {
 
 
  // Datenübergabe an Renderer mit dynamischen Objekt
- var env: any = new Object();
+ let env: any = new Object();
  env.version = process.versions['electron'];
  env.os = process.platform;
  env.appversion = app.getVersion();
  (<any>win).env = env;
 
-console.log("Lade Index.html...");
-
+writeLog("Electron/Main:Lade Index.html...");
 
  // and load the index.html of the app.
  win.loadURL(url.format({
@@ -60,47 +67,70 @@ console.log("Lade Index.html...");
 
  let contents = win.webContents;
 
- // =================== Anwendungsmenü einbinden
+ // =================== Anwendungsmenü
+ writeLog("Electron/Main:Anwendungsmenü erstellen...");
  var menuTemplate = MiracleListAppMenu.CreateMenu(app, win);
  const menu = Menu.buildFromTemplate(menuTemplate);
  Menu.setApplicationMenu(menu);
 
+ // =================== Reaktion auf Events
+ writeLog("Electron/Main:Event Handler erstellen...");
+ ipcMain.on('export', (event, arg) => {
+  console.log("export-event", arg);
+  writeLog("export-event!");
+  let file = path.join(app.getPath("documents"), 'miraclelist_export.json');
+  let text = JSON.stringify(arg);
+  console.log("Export-Datei", file);
+  writeLog("export-Datei: " + file);
+  fs.appendFile(file, text, (err) => {
+   if (err) throw err;
+  });
+  console.log("Export: OK!", file);
+  event.sender.send('export-reply', 'Aufgaben exportiert in Datei ' + file);
+ });
+
  // =================== Traymenü erstellen
+
  // siehe auch https://github.com/electron/electron/blob/master/docs/api/tray.md
- let tray = new Tray(favicon);
- const contextMenu: Electron.Menu = Menu.buildFromTemplate([
-  {
-   label: 'Über diese Anwendung', click: () => {
-   const options : ShowMessageBoxOptions = {
-       type: 'info',
-       title: 'Cross-Plattform-Desktop-Variante der Beispielanwendung MiracleList',
-       buttons: ['Ok'],
-       message: '(C) Dr. Holger Schwichtenberg 2017\nDetails siehe Anwendungsmenü!\nSystembenutzer: ' + username.sync() + ''
-   }
-   dialog.showMessageBox(options, function() {})
-  }
-  },
-  {
-   label: 'Verstecken', click: () => {  win.minimize(); }
-  },
-  {
-   label: 'Wiederherstellen', click: () => {  win.restore(); }
-  },
-  {
-   label: 'Maximieren', click: () => {  win.maximize(); }
-  },
-  {
-   label: 'Abmelden', click: () => { contents.send('logout', {msg: ''});  }
-  },
-  {
-   label: 'Beenden', click: () => {  app.quit(); }
-  },
+ try {
+  writeLog("Electron/Main:Traymenü erstellen...");
+  let tray = new Tray(icon);
+  const contextMenu: Electron.Menu = Menu.buildFromTemplate([
+   {
+    label: 'Über diese Anwendung', click: () => {
+     const options: ShowMessageBoxOptions = {
+      type: 'info',
+      title: 'Cross-Plattform-Desktop-Variante der Beispielanwendung MiracleList',
+      buttons: ['Ok'],
+      message: '(C) Dr. Holger Schwichtenberg 2017\nDetails siehe Anwendungsmenü!\nSystembenutzer: ' + username.sync() + ''
+     }
+     dialog.showMessageBox(options, function () { })
+    }
+   },
+   {
+    label: 'Verstecken', click: () => { win.minimize(); }
+   },
+   {
+    label: 'Wiederherstellen', click: () => { win.restore(); }
+   },
+   {
+    label: 'Maximieren', click: () => { win.maximize(); }
+   },
+   {
+    label: 'Abmelden', click: () => { contents.send('logout', { msg: '' }); }
+   },
+   {
+    label: 'Beenden', click: () => { app.quit(); }
+   },
 
- ]);
+  ]);
 
- tray.setToolTip('MiracleList');
- tray.setContextMenu(contextMenu);
-
+  tray.setToolTip('MiracleList');
+  tray.setContextMenu(contextMenu);
+ }
+ catch (err) {
+  writeLog("Electron/Main:Tray-Fehler: " + err.message)
+ }
  // =================== Globaler Shortcut
  // TODO: https://github.com/electron/electron/blob/master/docs/api/global-shortcut.md
 
@@ -115,19 +145,7 @@ console.log("Lade Index.html...");
   win = null;
  });
 
-// -------------- Reaktion auf Events
-
- ipcMain.on('export', (event, arg) => {
-  console.log("export-event", arg);
-  let file = path.join(app.getPath("documents"), 'miraclelist_export.json');
-  let text = JSON.stringify(arg);
-  fs.appendFile(file,text, (err) => {
-   if (err) throw err;
-  });
-  event.sender.send('export-reply', 'Aufgaben exportiert in Datei ' + file);
- });
-
- console.log("Electron/Main:createWindow END");
+ writeLog("Electron/Main:createWindow END");
 }
 
 // This method will be called when Electron has finished
@@ -137,7 +155,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
- writeLog("Anwendungsende");
+  writeLog("Electron/Main:window-all-closed");
  // On macOS it is common for applications and their menu bar
  // to stay active until the user quits explicitly with Cmd + Q
  if (process.platform !== 'darwin') {
@@ -149,17 +167,16 @@ app.on('activate', () => {
  // On macOS it's common to re-create a window in the app when the
  // dock icon is clicked and there are no other windows open.
  if (win === null) {
-  console.log("Electron/Main:activate");
+  writeLog("Electron/Main:activate");
   createWindow();
  }
 });
 
-function writeLog(logtext: string)
-{
- logtext = moment().format("DD.MM.YYYY HH:mm:ss") + ": " +  logtext + "\r\n";
+function writeLog(logtext: string) {
+ console.log(logtext);
+ logtext = moment().format("DD.MM.YYYY HH:mm:ss") + ": " + logtext + "\r\n";
  let logfile = path.join(app.getPath("documents"), 'miraclelist_log.txt')
- console.log(logfile);
- fs.appendFile(logfile,logtext , (err) => {
+ fs.appendFile(logfile, logtext, (err) => {
   if (err) throw err;
  });
 }
