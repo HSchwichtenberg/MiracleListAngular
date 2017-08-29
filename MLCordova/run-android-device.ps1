5110a041755f5cc0a0f6de4dff64bb98df548fb7
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-08-29 13:15:36 +0200

    Startskripte für Cordova App

---
+++ MLCordova/run-android-device.ps1
@@ -0,0 +1,10 @@
+
+"-------> Übersetzen für Cordova..."
+if ((get-location).Path -like "*MLCordova*") { cd ".." } 
+npm run build-cordova
+"-------> Übersetzen in Cordova..."
+cd MLCordova
+cordova build android
+"-------> Starten..."
+
+cordova run android -device 
