ce139b1d3868a2d9eb40701f105e0a265115a865
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-08-30 11:47:19 +0200

    fix im Deployskript

--- electron/deploy.ps1
+++ electron/deploy.ps1
@@ -1,5 +1,9 @@
 if ((get-location).Path -like "*electron*") { cd ".." } 
+"--> Angular-Produktions-Build für Electron nach temp_electron..."
 ng build --target=production --environment=prod --output-path=temp_electron --base-href . 
-npm run m-copy-electron 
+"--> Kopiere Zusatzdateien für Electron nach /temp_electron ..."
+npm run w-copy-electron 
+"--> Electron-Paket erstellen..."
 electron-packager temp_electron MiracleListElectron --platform=darwin,linux,win32 --arch=x64 --out=dist_electron/ --overwrite --icon=src/assets/favicon.ico  --asar
-"!!!FERTIG!!!"
+"--> !!!FERTIG!!!"
+& "H:\MiracleListClient\dist_electron\MiracleListElectron-win32-x64\MiracleListElectron.exe"
