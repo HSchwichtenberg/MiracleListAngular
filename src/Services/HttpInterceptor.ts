005f9c5fff108425fa2b094a89e7839d9ee76383
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-09-19 13:33:22 +0200

    f

--- src/Services/HttpInterceptor.ts
+++ src/Services/HttpInterceptor.ts
@@ -5,64 +5,66 @@
 // https://scotch.io/@kashyapmukkamala/using-http-interceptor-with-angular2
 
 import { CommunicationService } from './CommunicationService';
-import {Injectable} from "@angular/core";
-import { ConnectionBackend, RequestOptions, Request, RequestOptionsArgs, Response, Http, Headers} from "@angular/http";
-import {Observable} from "rxjs/Rx";
-import {environment} from "../environments/environment";
+import { Injectable } from "@angular/core";
+import { ConnectionBackend, RequestOptions, Request, RequestOptionsArgs, Response, Http, Headers } from "@angular/http";
+import { Observable } from "rxjs/Rx";
+import { environment } from "../environments/environment";
 
 @Injectable()
 export class HttpInterceptor extends Http {
-    constructor(private communicationService: CommunicationService, backend: ConnectionBackend, defaultOptions: RequestOptions) {
-        super(backend, defaultOptions);
-    }
+ constructor(private communicationService: CommunicationService, backend: ConnectionBackend, defaultOptions: RequestOptions) {
+  super(backend, defaultOptions);
+ }
 
-    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
-        return super.request(url, this.getRequestOptionArgs(options));
-    }
+ request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+  // console.log("---------> request");
+  return super.request(url, this.getRequestOptionArgs(options));
+ }
 
-    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-           url = this.updateUrl(url);
-        return super.get(url, this.getRequestOptionArgs(options));
-    }
+ get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+  url = this.updateUrl(url);
+  // console.log("---------> get");
+  return super.get(url, this.getRequestOptionArgs(options));
+ }
 
-    post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
-        url = this.updateUrl(url);
-        return super.post(url, body, this.getRequestOptionArgs(options));
-    }
+ post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+  url = this.updateUrl(url);
+  return super.post(url, body, this.getRequestOptionArgs(options));
+ }
 
-    put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
-        url = this.updateUrl(url);
-        return super.put(url, body, this.getRequestOptionArgs(options));
-    }
+ put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+  url = this.updateUrl(url);
+  return super.put(url, body, this.getRequestOptionArgs(options));
+ }
 
-    delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        url = this.updateUrl(url);
-        return super.delete(url, this.getRequestOptionArgs(options));
-    }
+ delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+  url = this.updateUrl(url);
+  return super.delete(url, this.getRequestOptionArgs(options));
+ }
 
-    private updateUrl(req: string) {
-        return  req;
-    }
+ // keine Funktion aktuell. Könnte aber die Url verändern :-)
+ private updateUrl(req: string) {
+  return req;
+ }
 
-    private getRequestOptionArgs(options?: RequestOptionsArgs) : RequestOptionsArgs {
-      
-     if (options == null) {
-            options = new RequestOptions();
-        }
-        if (options.headers == null) {
-            options.headers = new Headers();
-        }
+ // Erweitert die http-Anfrage um Token
+ private getRequestOptionArgs(options?: RequestOptionsArgs): RequestOptionsArgs {
 
-        if (this.communicationService.token)
-         {
-          // console.log("********* HttpInterceptor: Token injiziert: " + this.communicationService.token);
-          options.headers.set('ML-AuthToken', this.communicationService.token);
-        }
-        else
-         {
-        // console.log("********* HttpInterceptor: Kein Token!");
-         }
+  if (options == null) {
+   options = new RequestOptions();
+  }
+  if (options.headers == null) {
+   options.headers = new Headers();
+  }
 
-        return options;
-    }
+  if (this.communicationService.token) {
+   // console.log("********* HttpInterceptor: Token injiziert: " + this.communicationService.token);
+   options.headers.set('ML-AuthToken', this.communicationService.token);
+  }
+  else {
+   // console.log("********* HttpInterceptor: Kein Token!");
+  }
+
+  return options;
+ }
 }
