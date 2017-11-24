5ed4219f0b213e69b533e5d82990aa78ab0a1a64
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-24 20:43:46 +0100

    Skript verbessert

--- Electron/build-e.ps1
+++ Electron/build-e.ps1
@@ -1,6 +1,5 @@
 $erroractionpreference = "stop"
 "Kopiere Zusatzdateien für Electron nach /temp_electron ..."
 if ((get-location).Path -notlike "*electron*") { cd "electron" }
-copy-item * ..\temp_electron -Recurse -Verbose
+copy-item * ..\temp_electron -Recurse -Force -exclude *.ps1
 .\run-windows.ps1
-
