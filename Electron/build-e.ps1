e218014e375b228978f9529da050369ff1664d69
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-27 16:28:22 +0100

    Aufräumen

--- Electron/build-e.ps1
+++ Electron/build-e.ps1
@@ -1,6 +1,7 @@
 $erroractionpreference = "stop"
 "Kopiere Zusatzdateien für Electron nach /temp_electron ..."
 if ((get-location).Path -notlike "*electron*") { cd "electron" }
-copy-item src\* ..\temp_electron -Force
 copy-item .\package.json ..\temp_electron -Force
+copy-item src\* ..\temp_electron -Force
+
 .\run-windows.ps1
