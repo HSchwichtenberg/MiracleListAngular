5d79bbd4e9a9145632d9d37bddfbb5384ab18377
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-27 19:46:19 +0100

    fix

--- Electron/build-ng+e.ps1
+++ Electron/build-ng+e.ps1
@@ -3,6 +3,9 @@ if ((get-location).Path -like "*electron*") { cd ".." }
 "Übersetze Angular-App für Electron nach /temp_electron ..."
 ng build --output-path=temp_electron --base-href .
 "Kopiere Dateien..."
-copy-item Electron temp_electron  -Force -exclude *.ps1 -Recurse
 cd "Electron"
+copy-item .\package.json ..\temp_electron -Force
+copy-item src\* ..\temp_electron -Force
+"Kopiere Node-Module..."
+copy-item node_modules\* ..\temp_electron\node_modules -Force -Recurse
 .\build-e.ps1
