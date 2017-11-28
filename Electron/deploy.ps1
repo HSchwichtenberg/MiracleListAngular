2834e7e05575c3d3cd2febf6e89d99fa69c7ca0f
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-28 10:08:14 +0100

    Korrigiert für neue Dateisystemstruktur

--- Electron/deploy.ps1
+++ Electron/deploy.ps1
@@ -1,9 +1,13 @@
-if ((get-location).Path -like "*electron*") { cd ".." } 
+$erroractionpreference = "stop"
+if ((get-location).Path -like "*electron*") { cd ".." }
 "--> Angular-Produktions-Build für Electron nach temp_electron..."
-ng build --target=production --environment=prod --output-path=temp_electron --base-href . 
+ng build --target=production --environment=prod --output-path=temp_electron --base-href .
 "--> Kopiere Zusatzdateien für Electron nach /temp_electron ..."
-npm run w-copy-electron 
+copy-item Electron\package.json temp_electron -Force
+copy-item Electron\src\*.js temp_electron -Force
+"Kopiere Node-Module..."
+copy-item Electron\node_modules\* temp_electron\node_modules -Force -Recurse
 "--> Electron-Paket erstellen..."
-electron-packager temp_electron MiracleListElectron --platform=darwin,linux,win32 --arch=x64 --out=dist_electron/ --overwrite --icon=src/assets/favicon.ico  --asar
+electron-packager temp_electron MiracleListElectron --platform=darwin,linux,win32 --arch=x64 --out=dist_electron/ --overwrite --icon=Electron/src/img/assets/favicon.ico --asar
 "--> !!!FERTIG!!!"
 & "H:\MiracleListClient\dist_electron\MiracleListElectron-win32-x64\MiracleListElectron.exe"
