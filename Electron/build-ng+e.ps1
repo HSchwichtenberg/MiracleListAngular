c7bf08c7866bf19a682167d617614e5eb93300de
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-28 23:10:55 +0100

    Skripte besser strukturiert

--- Electron/build-ng+e.ps1
+++ Electron/build-ng+e.ps1
@@ -4,6 +4,4 @@ cd $PSScriptRoot\..
 "Übersetze Angular-App für Electron nach /temp_electron ..."
 ng build --output-path=temp_electron --base-href .
 Set-Location $lok
-"Erstelle Junction für Node-Module..."
-New-Item -ItemType Junction -Name node_modules -Value $PSScriptRoot\node_modules -Path $PSScriptRoot\..\temp_electron
 .\build-e.ps1
