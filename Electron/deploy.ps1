de4fbb74a43fad22ef41c74b4831003f543c7e46
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-28 23:44:44 +0100

    fix

--- Electron/deploy.ps1
+++ Electron/deploy.ps1
@@ -2,9 +2,8 @@ $erroractionpreference = "stop"
 $lok = Get-Location
 cd $PSScriptRoot\..
 "--> Angular-Produktions-Build für Electron nach temp_electron..."
-#ng build --target=production --environment=prod --output-path=temp_electron --base-href .
-cd $PSScriptRoot
-.\copy-electronmain.ps1
+ng build --target=production --environment=prod --output-path=temp_electron --base-href .
+& "$PSScriptRoot\copy-electronmain.ps1"
 cd $PSScriptRoot\..
 "--> Electron-Paket erstellen..."
 electron-packager temp_electron MiracleListElectron --platform=darwin,linux,win32 --arch=x64 --out=dist_electron/ --overwrite --icon=Electron/src/img/favicon.ico --asar
