90a9a451d21c2849f22e22a2f986993ac459a749
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-11-28 23:57:00 +0100

    fix

--- Electron/deploy.ps1
+++ Electron/deploy.ps1
@@ -6,7 +6,7 @@ ng build --target=production --environment=prod --output-path=temp_electron --ba
 & "$PSScriptRoot\copy-electronmain.ps1"
 cd $PSScriptRoot\..
 "--> Electron-Paket erstellen..."
-electron-packager temp_electron MiracleListElectron --platform=darwin,linux,win32 --arch=x64 --out=dist_electron/ --overwrite --icon=Electron/src/img/favicon.ico --asar
+electron-packager temp_electron MiracleListElectron --platform=win32 --arch=x64 --out=dist_electron2/ --overwrite --icon=Electron/src/img/favicon.ico --asar #darwin,linux
 "--> !!!FERTIG!!!"
 cd $lok
-& "$PSScriptRoot\..\dist_electron\MiracleListElectron-win32-x64\MiracleListElectron.exe"
+& "$PSScriptRoot\..\dist_electron2\MiracleListElectron-win32-x64\MiracleListElectron.exe"
