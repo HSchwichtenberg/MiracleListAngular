aa6e739bd19b8909c224045e935c5ecae6795d72
Dr. Holger Schwichtenberg <HS@IT-Visions.local>
2017-12-05 12:33:46 +0100

    fix

--- Cordova/build-ng.ps1
+++ Cordova/build-ng.ps1
@@ -1,6 +1,6 @@
 $erroractionpreference = "stop"
 $lok = Get-Location
-"Uebersetze Angular-App für Cordova nach /Cordova/www ..."
+"Uebersetze Angular-App fuer Cordova nach /Cordova/www ..."
 cd $PSScriptRoot\..
 ng build --base-href ./ --output-path Cordova/www/
 cd $lok
