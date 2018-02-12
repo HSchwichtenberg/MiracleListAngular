c33b2e3d5b143f7b1b2d39f415bf0c870a2679e1
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-02-13 00:24:34 +0100

    +Unit tests und jUnit-Ausgabe via https://github.com/karma-runner/karma-junit-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -34,12 +34,23 @@ module.exports = function(config) {
         angularCli: {
             environment: 'dev'
         },
-        reporters: config.angularCli && config.angularCli.codeCoverage ? ['progress', 'coverage-istanbul'] : ['progress', 'kjhtml'],
+        reporters: config.angularCli && config.angularCli.codeCoverage ? ['progress', 'coverage-istanbul', 'junit'] : ['progress', 'kjhtml'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['Chrome'],
-        singleRun: false
+        singleRun: false,
+          // the default configuration
+    junitReporter: {
+     outputDir: '', // results will be saved as $outputDir/$browserName.xml
+     outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+     suite: '', // suite will become the package name attribute in xml testsuite element
+     useBrowserName: true, // add browser name to report and classes names
+     nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+     classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+     properties: {}, // key value pair of properties to add to the <properties> section of the report
+     xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+   }
     });
 };
