9a753b49dce6d71b28b2bdd8857d9f29bb5f4b31
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-06-10 17:41:14 +0200

    Wegen einer Änderung in Angular CLI 6 muss der Junction Point nun vorher entfernt werden, da sonst der ganze Ordner /Electron/nodes_modules bei bg buld verschwindet

--- Electron/build-ng+e.ps1
+++ Electron/build-ng+e.ps1
@@ -1,6 +1,15 @@
 $erroractionpreference = "stop"
 $lok = Get-Location
 cd $PSScriptRoot\..
+
+if ((test-path $PSScriptRoot\..\temp_electron\node_modules))
+{
+ "Entferne Junction fuer node_modules"
+ $Directory = Get-Item $PSScriptRoot\..\temp_electron\node_modules
+ $Directory.FullName
+$Directory.Delete()
+}
+
 "Uebersetze Angular-App fuer Electron nach /temp_electron ..."
 ng build --output-path=temp_electron --base-href .
 Set-Location $lok
