148b27a429e6af91387a79c3a9d959a8eb0ae873
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-10-09 19:54:13 +0200

    +Release Date in URL.json

--- src/Services/AppLoadService.ts
+++ src/Services/AppLoadService.ts
@@ -2,26 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 import { environment } from '../environments/environment';
- 
+
 @Injectable()
 export class AppLoadService {
- 
+
  public static URL : string = "";
   constructor(private httpClient: HttpClient) { }
- 
+
   initializeApp(): Promise<any> {
     return new Promise((resolve, reject) => {
           console.log(`initializeApp:: inside promise`);
- 
+
           setTimeout(() => {
             console.log(`initializeApp:: inside setTimeout`);
             // doing something
- 
+
             resolve();
           }, 3000);
         });
   }
- 
+
   getSettings(): Promise<any> {
     console.log(`getSettings:: before http.get call`);
     var name = "API_BASE_URL";
@@ -32,16 +32,15 @@ export class AppLoadService {
       .then(settings => {
         console.log(`Settings from API: `, settings);
 
-
         AppLoadService.URL = settings[name];
         console.log(`GetConfig: ${name} aus Config=${AppLoadService.URL}`);
-      
+
         console.log(`APP_SETTINGS: `, settings);
- 
+
         return settings;
       }).catch(function(e) {  return null });
       // .finally(
-      //  function() {  
+      //  function() {
       //   if (!baseURL){
       //    baseURL = environment["name"];
       //    console.log(`GetConfig: ${name} aus Environment=${baseURL}`);
@@ -49,7 +48,7 @@ export class AppLoadService {
       //    AppLoadService.URL = baseURL;
       //   }
       // );
- 
+
     return promise;
   }
 }
